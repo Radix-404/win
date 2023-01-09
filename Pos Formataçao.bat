bd84f73cb5f3a6e1daf87cee782b6a1014c4307e
Gustavo <40724636+Radix-404@users.noreply.github.com>
2023-01-08 23:14:24 -0300

    update fix

--- Pos Formataçao.bat
+++ Pos Formataçao.bat
@@ -1,23 +1,43 @@
-winget install --id=Microsoft.dotnet -e  
-&& winget install --id=Microsoft.dotnetPreview -e  
-&& winget install --id=Microsoft.DotNet.SDK.6 -e  
-&& winget install --id=Microsoft.DotNet.SDK.3_1 -e  
-&& winget install --id=Microsoft.DirectX -e  
-&& winget install --id=Microsoft.VCRedist.2013.x86 -e  
-&& winget install --id=Microsoft.VCRedist.2013.x64 -e  
-&& winget install --id=Microsoft.VCRedist.2015+.x86 -e  
-&& winget install --id=Microsoft.VCRedist.2015+.x64 -e 
-&& winget install --id=Discord.Discord -e  
-&& winget install --id=Valve.Steam -e  
-&& winget install --id=EpicGames.EpicGamesLauncher -e  
-&& winget install --id=Ubisoft.Connect -e  
-&& winget install --id=Balena.Etcher -e  
-&& winget install --id=Notepad++.Notepad++ -e  
-&& winget install --id=qBittorrent.qBittorrent -e  
-&& winget install --id=Microsoft.DirectX -e  
-&& winget install --id=7zip.7zip -e  
-&& winget install --id=ElectronicArts.EADesktop -e  
-&& winget install --id=Microsoft.PowerToys -e  
-&& winget install --id=VideoLAN.VLC -e  
-&& winget install --id=GIMP.GIMP -e  
-&& winget install --id=BleachBit.BleachBit -e 
+winget install -e --id Microsoft.DotNet.Framework.DeveloperPack_4
+winget install -e --id Microsoft.DotNet.Runtime.7
+winget install -e --id Microsoft.DotNet.Runtime.3_1
+winget install -e --id Microsoft.VCRedist.2005.x86
+winget install -e --id Microsoft.VCRedist.2005.x64
+winget install -e --id Microsoft.VCRedist.2008.x86
+winget install -e --id Microsoft.VCRedist.2008.x64
+winget install -e --id Microsoft.VCRedist.2010.x86
+winget install -e --id Microsoft.VCRedist.2010.x64
+winget install -e --id Microsoft.VCRedist.2012.x86
+winget install -e --id Microsoft.VCRedist.2012.x64
+winget install -e --id=Microsoft.VCRedist.2013.x86  
+winget install -e --id=Microsoft.VCRedist.2013.x64   
+winget install -e --id Microsoft.VCRedist.2015+.x86
+winget install -e --id Microsoft.VCRedist.2015+.x64
+winget install -e --id=Discord.Discord 
+winget install -e --id=Valve.Steam 
+winget install -e --id=EpicGames.EpicGamesLauncher   
+winget install -e --id=Ubisoft.Connect 
+winget install -e --id=Balena.Etcher   
+winget install -e --id=Notepad++.Notepad++   
+winget install -e --id=qBittorrent.qBittorrent   
+winget install -e --id=Microsoft.DirectX 
+winget install -e --id=7zip.7zip 
+winget install -e --id=ElectronicArts.EADesktop   
+winget install -e --id=Microsoft.PowerToys
+winget install -e --id=VideoLAN.VLC 
+winget install -e --id=GIMP.GIMP  
+winget install -e --id=BleachBit.BleachBit  
+winget install -e --id Mozilla.Firefox
+
+bcdedit /set useplatformtick yes   
+bcdedit /set disabledynamictick yes
+bcdedit /deletevalue useplatformclock 
+Fsutil behavior query memoryusage
+Fsutil behavior set memory usage 2
+powercfg.exe /hibernate off
+Fsutil behavior query DisableDeleteNotify
+fsutil behavior set disabledeletenotify 0
+ipconfig /release
+ipconfig /renew
+ipconfig /flushdns
+netsh winsock reset
